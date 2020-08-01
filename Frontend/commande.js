7e4fc0a98163b1bc809da23fa059b93d5640c45a
Mickael VERMAND <mickael@mbp-de-mickael.home>
2020-08-01 13:01:43 +0200

    Requete commande

--- Frontend/commande.js
+++ Frontend/commande.js
@@ -8,13 +8,25 @@ document.getElementById("form").addEventListener("submit", function() {
     ville: document.querySelector("#ville").value,
     email: document.querySelector("#email").value
   }
-});
+  //Création du tableau de produits
+  let panier = localStorage.getItem("panier");
+  panier = JSON.parse(panier);
+  let products = [];
+  panier.produits.forEach(element => {
+    products.push(element._id);
+  })
+  console.log(products);
 
-//Création du tableau de produits
-let panier = localStorage.getItem("panier");
-panier = JSON.parse(panier);
-let products = [];
-panier.produits.forEach(element => {
-  products.push(element._id);
-})
-console.log(products);
+  //Envoi des données à l'API
+  let requete = new XMLHttpRequest();
+  let url = 'http://localhost:3000/api/cameras/order';
+  requete.open("POST", url);
+  requete.setRequestHeader("Content-Type", "application/json");
+
+  requete.addEventListener("load", function() {
+    alert(requete.responseText);
+  })
+
+  let envoi = JSON.stringify({contact,products});
+  requete.send(envoi);
+});
